087288bc511a3bb0773e5bf05e66b3b7a2605f29
Qitingshe <qitingshe@gmail.com>
2025-07-17 14:33:44 +0800

    user-id

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '1339d12e-327f-4dc7-b8bf-c0e603de158f';
+let userID = '80fe739e-467a-413c-a7a3-fd56728e8373';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
